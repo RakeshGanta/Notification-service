70e31e74be35acfb4919e8a2cafd7b6ac4fe03e2
Rakesh Ganta <gganta@guidewire.com>
2024-11-23 18:39:21 +0530

    vercel deployment changes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 // src/main.ts
 
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT || 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
